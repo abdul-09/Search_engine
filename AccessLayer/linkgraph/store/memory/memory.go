4856c4af5fdf32313fd62bd310fa16cfef9a11d2
abdul-09 <abdul.9.hussein@gmail.com>
2024-12-17 15:33:40 +0300

    boilerplate

---
+++ AccessLayer/linkgraph/store/memory/memory.go
@@ -0,0 +1,19 @@
+package memory
+
+import (
+	"github.com/google/uuid/"
+	"sync"
+
+)
+type edgeList []uuid.UUID
+
+type InMemoryGraph struct {
+	mu 				sync.RWMutex 
+
+	links 			map[uuid.UUID]*graph.Link
+	edges 			map[uuid.UUID]*graph.Edge
+
+	linkURLIndex 	map[string]*graph.Link
+	linkEdgeMap 	map[uuid.UUID]edgeList
+
+}
